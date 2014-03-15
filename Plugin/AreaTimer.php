7f2c6efc89926cd1ef2ed12e35cfb729b0738505
randall123459 <ryan.randall123459@gmail.com>
2014-03-15 08:56:58 +0000

    Update AreaTimer.php
    By Randall123459

--- Plugin/AreaTimer.php
+++ Plugin/AreaTimer.php
@@ -1,1 +1,10 @@
-//No Development Builds Posted Yet!
+<?php
+/*
+__PocketMine Plugin__
+name=AreaTimer
+description=Allows you only to be in a certain area for a certain time
+version=1.0
+author=Github Community
+class=AreaTimer
+apiversion=6,7,8,9,10,11,12
+*/
